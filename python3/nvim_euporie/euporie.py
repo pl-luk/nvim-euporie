b48da87ddf8c17c342da5f0f627901e279055842
pl-luk <lukas@smart-ies.de>
2023-03-23 13:57:10 +0100

    First try in launching euporie

--- python3/nvim_euporie/euporie.py
+++ python3/nvim_euporie/euporie.py
@@ -1,6 +1,9 @@
 import vim
+import euporie
+from importlib.metadata import entry_points
+
 
 class Euporie():
 
     def run(self):
-        pass
+       entry_points().select(name='notebook')[0].load() 
